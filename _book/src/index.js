9afc1b6dbd880ad1ea1c9df994c089eb9239de71
mengbaoqing <mengbaoqing@bailitop.com>
2018-10-11 17:04:44 +0800

    add _book

---
+++ _book/src/index.js
@@ -0,0 +1,16 @@
+Class FastCache {
+   constructor() {
+   	this.list = {};
+   }
+   set(key, value) {
+   	this.list[key] = value;
+   }
+   get(key) {
+   	return this.list[key];
+   }
+   clear() {
+   	this.list = {};
+   }
+}
+
+window.FastCache = FastCache;
